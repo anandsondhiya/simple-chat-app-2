7b8f4a92672b5eef158ead95b584c7accf8530fb
Anand Sondhiya <anandsondhiya09@gmail.com>
2023-08-24 14:30:38 +0530

    added emoji search and replace feature

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+const emojiDictionary = {
+    "smile": "😄",
+    "happy": "😃",
+    "react": "⚛️",
+    "woah": "😲",
+    "hey": "👋",
+    "lol": "😂",
+    "like": "❤️",
+    "congratulations": "🎉",
+    
+    // Add more word-emoji pairs as needed
+};
+
+
+
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -7,6 +22,8 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 
+
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
@@ -14,16 +31,31 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('A user connected');
     
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg); // Broadcast the message to all connected clients
-    });
+    // socket.on('chat message', (msg) => {
+        
+    //     io.emit('chat message', msg); // Broadcast the message to all connected clients
+    // });
     
+    socket.on('chat message', (msg) => {
+        const words = msg.split(' ');
+        const transformedWords = words.map(word => {
+            if (emojiDictionary[word])  {
+                return emojiDictionary[word];
+            }
+            return word;
+        });
+        const transformedMessage = transformedWords.join(' ');
+
+        io.emit('chat message', transformedMessage); // Broadcast the message to all connected clients
+    });
+
+
     socket.on('disconnect', () => {
         console.log('A user disconnected');
     });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
